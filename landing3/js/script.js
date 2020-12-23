824c4758f8eef2a7852a13fae90bbfa40a8d8e0c
Chinedu Ukpe <chinedu_ukpe@outlook.com>
2020-12-23 22:12:48 +0100

    Added GSAP animation to landing page

--- landing3/js/script.js
+++ landing3/js/script.js
@@ -1,11 +1,32 @@
 (function(){
     const toggleNav = function(){
         nav.classList.toggle('show');
-        console.log("Toggling nav")
     }
     console.log("app loaded")
     let nav = document.querySelector('nav');
     document.querySelector('.nav-toggle').addEventListener('click',toggleNav);
     document.querySelector('.close-nav').addEventListener('click',toggleNav);
 
+    // ANIMATION
+    let tl = gsap.timeline();
+    tl.from('.hero-content h1',1,{
+        duration: 2,
+        x: - 1000,
+        // ease: '',
+    })
+    .from('.hero-content p',.6,{
+        x: -1000
+    })
+    .from('.hero-content small',1,{
+        x: -1000
+    },'-=.5')
+    .from('.hero-content .learn-btn',1,{
+        height: 0
+    },"-=.6")
+    .from('.hero-gallery-img',1,{
+        scale: 0,
+        ease: "back.out"
+    },"-=1")
+
+    // END ANIMATION
 })()
